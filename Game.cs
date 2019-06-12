c1e420746bef5bc9f9e4bf5fc598f95477ad8b97
Uxeron <vgabrielius123@gmail.com>
2019-06-12 17:08:19 +0300

    Added input parsing, ability to discard cards

--- Game.cs
+++ Game.cs
@@ -20,12 +20,71 @@ namespace JacksOrBetter
                 cards.Add(new Card(Symbols[random.Next(Symbols.Length)], Numbers[random.Next(Numbers.Length)]));
             
             DrawCards();
+
+            List<int> parsedNumbers;
+
+            // Read input until correct input is received.
+            while (true)
+            {
+                Console.WriteLine("Enter numbers of cards to discard (space separated): ");
+
+                if (GetInput(out parsedNumbers))
+                    break;
+
+                Console.WriteLine("Error, invalid input");
+            }
+            
+
+            // Shuffle the selected cards
+            foreach (var number in parsedNumbers)
+                cards[number] = new Card(Symbols[random.Next(Symbols.Length)], Numbers[random.Next(Numbers.Length)]);
+            
+
+            DrawCards();
+
+        }
+
+
+        bool GetInput(out List< int > parsedNumbers) {
+            parsedNumbers = new List< int > ();
+            var input = Console.ReadLine().Split(" ");
+            int parsedNumber;
+
+            foreach (var number in input)
+            {
+                // Check if input is a number.
+                if (Int32.TryParse(number, out parsedNumber))
+                {
+                    // Input is 1-based, change it to 0-based.
+                    parsedNumber--;
+
+                    // Make sure number is within bounds.
+                    if (0 <= parsedNumber && parsedNumber < CardCount)
+                    {
+                        parsedNumbers.Add(parsedNumber);
+                    }
+                    else
+                    {
+                        parsedNumbers.Clear();
+                        return false;
+                    }
+                }
+                else
+                {
+                    parsedNumbers.Clear();
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         void DrawCards() 
         {
             // Clear the screen.
             Console.Clear();
+
+            Console.WriteLine("Your current hand: ");
 
             // Draw card indexes.
             for (int i = 0; i < CardCount; i++)
@@ -50,6 +109,8 @@ namespace JacksOrBetter
             // Draw bottom parts of all cards.
             foreach (var card in cards)
                 Console.Write(card.Bottom);
+            Console.WriteLine();
+
             Console.WriteLine();
         }
     }
