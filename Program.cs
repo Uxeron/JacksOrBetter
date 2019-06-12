999822eb74da4ca816492e710cbab2c3e94df88d
Uxeron <vgabrielius123@gmail.com>
2019-06-12 13:49:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JacksOrBetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
