ed2baabe37a0c71acd69dd148b804a4ab2d2fa76
Uxeron <vgabrielius123@gmail.com>
2019-06-12 22:02:42 +0300

    Fixed being able to draw multiple of the same card

--- Game.cs
+++ Game.cs
@@ -9,25 +9,31 @@ namespace JacksOrBetter
         private const string Symbols = "♠♥♦♣";
         private const string Numbers = "A23456789TJQK";
 
-        // Symbol, number/type.
-        private List< Card > cards = new List< Card > {};
+        private List< Card > deck = new List< Card > ();
+        private List< Card > hand = new List< Card > ();
         private Random random = new Random();
 
         public void Run()
         {
-            cards.Clear();
-            // Generate cards.
+            hand.Clear();
+            MakeNewDeck();
+
+            // Generate hand.
             for (int i = 0; i < CardCount; i++)
-                cards.Add(new Card(Symbols[random.Next(Symbols.Length)], Numbers[random.Next(Numbers.Length)]));
+            {
+                var card = deck[random.Next(deck.Count)];
+                deck.Remove(card);
+                hand.Add(card);
+            }
             
             DrawCards();
 
-            var parsedNumbers = new List<int>();
+            var parsedNumbers = new List< int >();
 
             // Read input until correct input is received.
             while (true)
             {
-                Console.WriteLine("Enter numbers of cards to discard (space separated): ");
+                Console.WriteLine("Enter numbers of hand to discard (space separated): ");
 
                 if (GetInput(parsedNumbers))
                     break;
@@ -35,18 +41,34 @@ namespace JacksOrBetter
                 Console.WriteLine("Error, invalid input");
             }
             
-
             // Shuffle the selected cards.
             foreach (var number in parsedNumbers)
-                cards[number] = new Card(Symbols[random.Next(Symbols.Length)], Numbers[random.Next(Numbers.Length)]);
-            
+            {
+                var card = hand[number];
+                hand.Remove(card);
+                deck.Add(card);
+
+                card = deck[random.Next(deck.Count)];
+                deck.Remove(card);
+                hand.Insert(number, card);
+            }
 
             DrawCards();
 
-            var evaluator = new HandEvaluator(cards);
+            var evaluator = new HandEvaluator(hand);
             var values = evaluator.Evaluate();
 
             Console.WriteLine("You drew {0} and won {1} coins", values.Item1, values.Item2);
+        }
+
+
+        private void MakeNewDeck() 
+        {
+            deck.Clear();
+
+            foreach (var symbol in Symbols)
+                foreach (var number in Numbers)
+                    deck.Add(new Card(symbol, number));
         }
 
 
@@ -68,7 +90,7 @@ namespace JacksOrBetter
                 // Check if input is a number.
                 if (!Int32.TryParse(number, out parsedNumber))
                     return false;
-                
+
                 // Make sure number is within bounds.
                 if (1 > parsedNumber || parsedNumber > CardCount)
                     return false;
@@ -87,22 +109,22 @@ namespace JacksOrBetter
             Console.WriteLine("Your current hand: ");
 
             // Draw top parts of all cards.
-            foreach (var card in cards)
+            foreach (var card in hand)
                 Console.Write(card.Top);
             Console.WriteLine();
 
             // Draw middle top parts of all cards.
-            foreach (var card in cards)
+            foreach (var card in hand)
                 Console.Write(card.MiddleTop);
             Console.WriteLine();
 
             // Draw middle bottom parts of all cards.
-            foreach (var card in cards)
+            foreach (var card in hand)
                 Console.Write(card.MiddleBottom);
             Console.WriteLine();
 
             // Draw bottom parts of all cards.
-            foreach (var card in cards)
+            foreach (var card in hand)
                 Console.Write(card.Bottom);
             Console.WriteLine();
 
